45a42aab7b4d74b0ff9ee6b62b6a3d556414c1f9
fregnani123 <fabianofregnani@hotmail.com>
2023-09-23 19:25:16 -0300

    css em andamento

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import './App.css'
-import exit from './assets/imagens gestaoLite/sair.png'
+import exit from './assets/imagens gestaoLite/sair.png';
+import imgMenu from './assets/imagens gestaoLite/menu-aberto.png'
 
 function App() {
  
   return (
     <body>
       <div className='menu'>
-        <h4 className='nameStore'>Capim Dourado Saboaria</h4>
+        <h4 className='nameStore'><img src={imgMenu} className='imgMenu' /> <span className='nameStoreS'>Gestão Lite</span></h4>
         <h4 className='nameMenu'>Menu</h4>
         <ul>
           <li className='listaMenu'>Realizar Venda</li>
@@ -14,11 +15,13 @@ function App() {
          
           <li className='listaMenu'>Detalhes de Venda</li>
           <li className='listaMenu'>Controle de Entrada/Saída</li>
-          <li className='listaMenu' id='exit'>
-            <img src={exit} className='imgExit' />
-            <span className='textoSair'>Exit</span>
-          </li>
         </ul>
+
+        <p className='exit'>
+          <img src={exit} className='imgExit' />
+          <p className='textoSair'>Sair</p>
+        </p>
+
       </div>
       <div className='menuScreen'></div>
     </body>
