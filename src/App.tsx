b79dcd01475bb7f1c39689c7695894e8de10f72d
fregnani123 <fabianofregnani@hotmail.com>
2023-09-24 17:16:37 -0300

    front end Menu prontos

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import './App.css'
-import exit from './assets/imagens gestaoLite/sair.png';
-import imgMenu from './assets/imagens gestaoLite/menu-aberto.png'
+import './App.css';
+import imgExit from './assets/imagens gestaoLite/sair.png';
+import imgMenu from './assets/imagens gestaoLite/menu-aberto.png';
+import imgVendas from './assets/imagens gestaoLite/carrinho-de-compras.png';
+import imgCadastroProduto from './assets/imagens gestaoLite/cadastro de produtos.png';
+import imgDetalhesVendas from './assets/imagens gestaoLite/detalhes vendas.png';
+import imgEntradaSaida from './assets/imagens gestaoLite/grafico-de-barras.png';
 
 function App() {
  
   return (
     <body>
+      <div className='container'>
+        
       <div className='menu'>
         <h4 className='nameStore'><img src={imgMenu} className='imgMenu' /> <span className='nameStoreS'>Gestão Lite</span></h4>
         <h4 className='nameMenu'>Menu</h4>
@@ -18,12 +24,27 @@ function App() {
         </ul>
 
         <p className='exit'>
-          <img src={exit} className='imgExit' />
+          <img src={imgExit} className='imgExit' />
           <p className='textoSair'>Sair</p>
         </p>
 
       </div>
-      <div className='menuScreen'></div>
+
+        <div className='menuScreen'>
+          
+        <ul className='ulMenuScreen'>
+            <li className='realizarVenda'><p>Realizar Venda</p><img className='imgClass' src={imgVendas} alt="" />
+            </li>
+            <li className='cadastroProdutos'><p>Cadastro de Produtos</p><img className='imgClass' src={imgCadastroProduto} alt="" /></li>
+        </ul> 
+          <ul className='ulMenuScreen'>
+            <li className='detalhesVenda'><p>Realizar Venda</p><img className='imgClass' src={imgDetalhesVendas} alt="" /></li>
+            <li className='entradaSaida'><p>Realizar Venda</p><img className='imgClass' src={imgEntradaSaida} alt="" /></li>
+          </ul>
+          
+      </div>
+
+   </div>
     </body>
     
     
