9554be1fd12d3b8f45eef0e531d98f300932a4aa
fregnani123 <fabianofregnani@hotmail.com>
2023-09-24 22:24:55 -0300

    pagina de login finalizada interface

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,13 +1,17 @@
 import '../components/index.css'
+import imgMenu from '../assets/imagens gestaoLite/f.png'
 
 function PaginaLogin() {
-    return (<>
+    return (<><h1 className='logo'><img className='imgMenuLogin' src={ imgMenu} /> Gestão Lite</h1>
       <form>
-            <h1>Login</h1> 
-            <input type='text' placeholder='Digite o seu usuário' />
-            <input type='password' placeholder='Digite a sua senha' />
+            <h1 >Login</h1> 
+            <input type='text' placeholder='Digite o seu usuário' required />
+            <input type='password' placeholder='Digite a sua senha' required/>
             <input type='submit'/>
-      </form>
+        </form>
+        <footer>
+            <p className="copy">© 2023 Fabiano Fregnani - Front-End Developer.</p>
+        </footer>
     </>)
 }
 
