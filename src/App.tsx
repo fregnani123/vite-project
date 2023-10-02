810682c77707bdaf307df0bca2e550d2c23b602e
fregnani123 <fabianofregnani@hotmail.com>
2023-10-02 19:11:50 -0300

    login testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
-import MenuPage from './components/Menu'
+import { useState } from 'react';
+import MenuPage from './components/Menu';
 import PaginaLogin from './components/Login';
-import './App.css'
-
-import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
   return (
     <div className='app'>
       <Router>
         <Routes>
-          <Route path='/' element={<PaginaLogin/>} />
-          <Route path='/painel' element={<MenuPage></MenuPage>}/>
+          <Route path='/' element={<PaginaLogin setIsAuthenticated={setIsAuthenticated} />} />
+          <Route
+            path='/painel'
+            element={isAuthenticated ? <MenuPage /> : <Navigate to='/' />}
+          />
         </Routes>
       </Router>
     </div>
-  )
+  );
 }
+
 export default App;
+
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,33 +1,63 @@
 import '../components/Login.css'
 import imgMenu from '../assets/imagens gestaoLite/f.png';
-import React, { useState } from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-function PaginaLogin() {
-    
-    const usuario = 'Fabiano';
-    const senha = 1308;
-
-
-    const [usuarioInput, setUsuario] = useState('')
-    const [senhaInput, setSenha] = useState('')
-
-    return (<><h1 className='logo'><img className='imgMenuLogin' src={ imgMenu} /> Gestão Lite</h1>
-        <form>
-            <h1 >Login {usuarioInput}</h1> 
-            <input type='text' placeholder='Digite o seu usuário'
-                value={usuarioInput} onChange={(e) => {
-                setUsuario(e.target.value)
-            } }
-            required />
-            <input type='password' placeholder='Digite a sua senha' value={senhaInput} onChange={(e) => {
-                setSenha(e.target.value)
-            }} required/>
-            <button className='logar'>Logar</button>
-        </form>
-        <footer>
-            <p className="copy">© 2023 Fabiano Fregnani - Front-End Developer.</p>
-        </footer>
-    </>)
+interface PaginaLoginProps {
+    setIsAuthenticated: (value: boolean) => void; // Define o tipo esperado para setIsAuthenticated
 }
 
-export default PaginaLogin
+function PaginaLogin({ setIsAuthenticated }: PaginaLoginProps ) {
+    const Login = useNavigate();
+    const [usuarioInput, setUsuario] = useState('')
+    const [senhaInput, setSenha] = useState('');
+    const keySenha = 'adm';
+    const keyUsuario = 'adm'
+
+    return (
+        <>
+            <h1 className='logo'><img className='imgMenuLogin' src={imgMenu} /> Gestão Lite</h1>
+            <form>
+                <h1 >Login</h1>
+                <input
+                    type='text'
+                    placeholder='Digite o seu usuário'
+                    value={usuarioInput}
+                    onChange={(e) => {
+                        setUsuario(e.target.value)
+                    }}
+                    required
+                />
+                <input
+                    type='password'
+                    placeholder='Digite a sua senha'
+                    value={senhaInput}
+                    onChange={(e) => {
+                        setSenha(e.target.value)
+                    }}
+                    required
+                />
+
+                <button
+                    className='logar'
+                    onClick={() => {
+                        if (usuarioInput === keyUsuario && senhaInput === keySenha) {
+                            console.log('ok');
+                            setIsAuthenticated(true);
+                            Login('/painel'); // Redireciona para a página MenuPage;
+                        } else {
+                            console.log('erro')
+                        }
+                    }}
+                >
+                    Logar
+                </button>
+            </form>
+            <footer>
+                <p className="copy">© 2023 Fabiano Fregnani - Front-End Developer.</p>
+            </footer>
+        </>
+    )
+}
+
+export default PaginaLogin;
