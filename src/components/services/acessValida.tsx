c70a1495abd2295706f130b5fe08830537994d15
fregnani123 <fabianofregnani@hotmail.com>
2023-10-03 19:43:22 -0300

    table

--- src/components/services/acessValida.tsx
+++ src/components/services/acessValida.tsx
@@ -3,19 +3,18 @@ import { useEffect, useState } from "react";
 
 interface Produto {
     nome: string;
-    preco: string;
+    preco: Number;
     descricao: string;
     categoria: string;
 }
 
 function MyComponent() {
     const [data, setData] = useState<Produto[]>([]); // Tipando o estado como um array de objetos Produto
-   console.log(data)
+    console.log(data)
     useEffect(() => {
         axios.get("http://localhost:3000/findProduto")
             .then(response => {
                 setData(response.data);
-                
             })
             .catch(error => {
                 console.error("Erro ao buscar dados:", error);
@@ -24,42 +23,29 @@ function MyComponent() {
 
     return (
         <div>
-            <p>
-                {data.map((produto, index) => (
-                    <span key={index}>{produto.nome}</span>
-                ))}
-            </p>
-            <h1>Produtos</h1>
             <table>
                 <thead>
                     <tr>
                         <th>Nome do Produto</th>
                         <th>Preço</th>
                         <th>Descrição</th>
+                        <th>Categoria</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>Produto 1</td>
-                        <td>R$ 50.00</td>
-                        <td>Descrição do Produto 1.</td>
-                    </tr>
-                    <tr>
-                        <td>Produto 2</td>
-                        <td>R$ 35.00</td>
-                        <td>Descrição do Produto 2.</td>
-                    </tr>
-                    <tr>
-                        <td>Produto 3</td>
-                        <td>R$ 70.00</td>
-                        <td>Descrição do Produto 3.</td>
-                    </tr>
+                    {data.map((produto, index) => (
+                        <tr key={index}>
+                            <td>{produto.nome}</td>
+                            <td>{produto.preco.toFixed(2)}</td>
+                            <td>{produto.descricao}</td>
+                            <td>{produto.categoria}</td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         </div>
     );
 }
-
 
 export default MyComponent;
 
