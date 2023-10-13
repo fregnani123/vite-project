520b33e1dd00ff0fadbcfa4315b6d1379cab80b4
fregnani123 <fabianofregnani@hotmail.com>
2023-10-13 19:29:20 -0300

    tela vendas React

--- src/components/views/SalesScreen.tsx
+++ src/components/views/SalesScreen.tsx
@@ -1,24 +1,63 @@
 import MenuToolbar from "../MenuToolbar";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import '../views/queryProdutos.css';
 
+interface Produto {
+    _id: string;
+    nome: string;
+    preco: number;
+    descricao: string;
+    categoria: string;
+    estoque: string;
+}
 
-const SalesScreen = () => {
-    return (<div className= "sales-container">
-        <MenuToolbar /><div className="sales-form"><h1 className="registroH1">Tela de Vendas</h1>
-            <div className="cupom">
-                <form className="cupom-form">
-                   <br></br><p className="relatorio"></p>
-                    <p></p>
-                    <label className="labelSales">Produto<input className="inputProduto" /></label>
-                    <label className="labelQtd">Qtd.<input type="number"/></label>
-                </form>
+function SalesScreen() {
+    const [nome, setNome] = useState('');
+    const [data, setData] = useState<Produto[]>([]);
+
+    // O URL da solicitação deve ser construído corretamente
+    const url = `http://localhost:3000/findProduto`;
+
+    useEffect(() => {
+        // Certifique-se de que a URL esteja corretamente construída
+        axios.get(url)
+            .then(response => {
+                setData(response.data);
+            })
+            .catch(error => {
+                console.error("Erro ao buscar dados:", error);
+            });
+    }, [nome]); // O efeito deve ser executado quando 'nome' é alterado.
+
+    return (
+        <div className="sales-container">
+            <MenuToolbar />
+            <div className="sales-form">
+                <h1 className="registroH1">Tela de Vendas</h1>
+                <div className="cupom">
+                    <form className="cupom-form">
+                        <br />
+                        <p className="relatorio">
+                        </p>
+                        <label className="labelSales">Código de Barras
+                            <input className="inputProduto"
+                            />
+                        </label>
+                        <label className="labelQtd">Qtd.
+                            <input type="number" />
+                        </label>
+
+                        <button className="buttonSales">Adicionar</button>
+                    </form>
+                </div>
+                <div className="h4Sales">
+                    <h4 className="h4register">TOTAL<br />VENDA</h4>
+                    <p className="spanVendas">R$ 0,00</p>
+                </div>
             </div>
-            <div className="h4Sales">
-                <h4 className="h4register" >TOTAL<br/>VENDA
-                </h4><p className="spanVendas">R$ 0,00</p>
-            </div>
-            </div>
-    </div>
+        </div>
     );
-};
+}
 
 export default SalesScreen;
