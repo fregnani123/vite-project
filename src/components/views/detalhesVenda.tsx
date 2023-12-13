ce9b9996fa5e68f8eea82f4e7f69a72b404de7eb
fregnani123 <fabianofregnani@hotmail.com>
2023-12-12 22:43:38 -0300

    melhorias tela de historico de vendas

--- src/components/views/detalhesVenda.tsx
+++ src/components/views/detalhesVenda.tsx
@@ -9,32 +9,32 @@ interface Relatorio {
     total: number;
     formaPagamento: string;
     dinheiroRecebido: number;
-    carrinho: []; // Uso do tipo Produto[] para representar um array de produtos
+    carrinho: [];
     dateVenda: Date;
+    _id: string;
 }
 
 function detalhesVendasScreen() {
-
     const [data, setData] = useState<Relatorio[]>([]);
+    const [dataInicio, setDataInicio] = useState(format(new Date(), 'yyyy-MM-dd'));
+    const [dataFim, setDataFim] = useState(format(new Date(), 'yyyy-MM-dd'));
 
-    //  console.log(data) 
-    
-    const URL = "http://localhost:3000/detalhes"
+  console.log(data)
+
+    const URL = "http://localhost:3000/detalhes";
 
     useEffect(() => {
-        axios.get(URL).then((response => {
-            setData(response.data);
-
-        })   
-        ).catch(error => console.log(error));
- 
+        axios.get(URL)
+            .then((response => {
+                setData(response.data);
+            }))
+            .catch(error => console.log(error));
     }, []);
 
-    
-    
-    // const dataInicial = new Date('2023-11-10T12:30:00.000Z');
-    // const dataLimite = new Date("2023-11-15T00:00:00.000Z");
-  
+    const formatDate = (date: Date) => {
+        return format(date, 'yyyy-MM-dd');
+    };
+
     return (
         <div className="detalhesContainer">
             <div className="menuDetalhes">
@@ -42,64 +42,52 @@ function detalhesVendasScreen() {
             </div>
             <h1 className="tituloDetalhes">Histórico de Vendas</h1>
 
-            {/* <div className="container">
-            <ul className="content">
+            <ul>
+                <li className="detalhesTable">
+                    <span className="codigoVenda">cod. da venda</span>
+                    <span className="dataDetalhes">Data venda</span>
+                    <span className="clienteDetalhes">Cliente</span>
+                    <span className="pagamentoDetalhes">Pagamento</span>
+                    <span className="totalDetalhes">Total</span>
+                    <span className="dinheiroDetalhes">Dinheiro Recebido</span>
+                    <span className="trocoDetalhes">Troco</span>
+                    <span className="correcaoSpan">.</span>
+                </li>
+            </ul>
+            <ul className="ulRelative">
                 {data
-                    .filter(objeto => new Date(objeto.dateVenda) >= dataInicial && new Date(objeto.dateVenda) <= dataLimite)
-                    .map((objeto, index) => (
-                        <li key={index}>
-                            <span>{objeto.cliente}</span>
-                            <span>{format(new Date(objeto.dateVenda), 'dd/MM/yyyy')}</span>
+                    .filter((venda) => {
+                        const vendaDate = new Date(venda.dateVenda);
+                        const inicioDate = new Date(dataInicio);
+                        const fimDate = new Date(dataFim);
+
+                        return vendaDate >= inicioDate && vendaDate <= fimDate;
+                    })
+                    .slice().reverse().map((venda, index) => (
+                        <li key={index} className="detalhesTable1">
+                            <span className="codigoVenda1">{venda._id}</span>
+                            <span className="dataDetalhes1">
+                                {format(new Date(venda.dateVenda), 'dd/MM/yyyy')}
+                            </span>
+                            <span className="clienteDetalhes1">{venda.cliente.toString()}</span>
+                            <span className="pagamentoDetalhes1">{venda.formaPagamento.toString()}</span>
+                            <span className="totalDetalhes1">{`R$ ${venda.total.toFixed(2)}`}</span>
+                            <span className="dinheiroDetalhes1">{`R$ ${venda.dinheiroRecebido.toFixed(2)}`}</span>
+                            <span className="trocoDetalhes1">{`R$ ${(Number(venda.dinheiroRecebido) - Number(venda.total)).toFixed(2)}`}</span>
+                            <span className="correcaoSpan">.</span>
                         </li>
                     ))}
-                </ul></div> */}
-                
-            <ul>
-            <li className="detalhesTable">
-                <span className="dataDetalhes">Data venda</span>
-                <span className="clienteDetalhes">Cliente</span>
-                <span className="pagamentoDetalhes">Pagamento</span>
-                <span className="totalDetalhes">Total</span>
-                <span className="dinheiroDetalhes">Dinheiro Recebido</span>
-                <span className="trocoDetalhes">Troco</span>
-                <span className="correcaoSpan">.</span>
-                </li></ul> 
-                    <ul className="ulRelative">
-                {data.slice().reverse().map((venda, index) => (
-                                <li key={index} className="detalhesTable1">
-                                    <span className="dataDetalhes1">
-                                        {format(new Date(venda.dateVenda), 'dd/MM/yyyy')}
-                                    </span>
-                                    <span className="clienteDetalhes1">{venda.cliente.toString()}</span>
-                                    <span className="pagamentoDetalhes1">{venda.formaPagamento.toString()}</span>
-                                    <span className="totalDetalhes1">{`R$ ${venda.total.toFixed(2)}`}</span>
-                                    <span className="dinheiroDetalhes1">{`R$ ${venda.dinheiroRecebido.toFixed(2)}`}</span>
-                                    <span className="trocoDetalhes1">{`R$ ${(Number(venda.dinheiroRecebido) - Number(venda.total)).toFixed(2)}`}</span>
-                                    <span className="correcaoSpan">.</span>
-                                </li>
-                            ))}     
-                    </ul>
-            <ul className="totalVendasMes" >
-                        <li>
-                    <span> {`Total vendas: R$ ${data.reduce(
-                        (acumulador, totalVendas) => acumulador + totalVendas.total,
-                        0
-                    ).toFixed(2)}`
-                    }</span><br />
-                    {data.filter( (venda,index) => {
-                        return index === 0
-                    }).map((venda, index) => (
-                        <span key={index}> 
-                            <span className="">
-                                {`Data de inicio: ${format(new Date(venda.dateVenda), 'dd/MM/yyyy')}`}
-                            </span>
-                            <span className="correcaoSpan">.</span>
-                        </span>
-                    ))}     
-                </li>
-                    </ul> 
-            </div>
-            )}
+            </ul>
 
+            <form className="totalVendasMes" >
+                <span>Selecione o período</span>
+                <label className="dataInicio">Início: <input type="date" value={dataInicio} onChange={(e) => setDataInicio(e.target.value)} /></label>
+                
+                <label className="dataFim">Fim: <input type="date" value={dataFim} onChange={(e) => setDataFim(e.target.value)} /></label>
+                
+            </form>
+        </div>
+    );
+}
 
 export default detalhesVendasScreen;
